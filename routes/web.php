c68b84fcf0e52304351d70ed328ef0aeee5010c2
Varogell <alvaroivanka@gmail.com>
2023-03-01 10:30:21 +0700

    commit2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,5 +30,7 @@ Route::get('/about', function (){
 Route::get('/articles/{id}', function ($id){
     echo "Halaman artikel dengan id $id";
 });
-
+Route::get('/',[HomeController::class,'index']);
+Route::get('/about',[AboutController::class, 'about']);
+Route::get('/articles/{id}',[ArticleController::class, 'articles']);
 
